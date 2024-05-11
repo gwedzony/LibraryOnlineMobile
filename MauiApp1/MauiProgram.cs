d6d639bd92eb96387604b4c50a9b39039da4724c
MikolajK7 <157275211+MikolajK7@users.noreply.github.com>
2024-05-11 15:06:54 +0200

    WIP-MadeDetailPageForBooks

--- MauiApp1/MauiProgram.cs
+++ MauiApp1/MauiProgram.cs
@@ -10,7 +10,9 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMaterialComponents()
             .ConfigureFonts(fonts =>
+            
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
